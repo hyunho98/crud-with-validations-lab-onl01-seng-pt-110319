7f0901a4ed4356ecef74dae91d286f0082e904d8
Alex Lee <hyunho98@gmail.com>
2020-05-04 11:51:34 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,4 +4,15 @@ class Song < ApplicationRecord
   validates :release_year, presence: true, if: :is_released?
   validates :release_year, :not_in_the_future
   validates :artist_name, presence: true
+
+  private
+
+  def repeat_title
+    songs = Song.find_by(title: title)
+    songs.each do |song|
+      if song.artist == artist && song.release_year == release_year
+        errors.add(:title, "song already exists")
+      end
+    end
+  end
 end
