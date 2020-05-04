8e7f3b39ab7e849a23146f1abec2b10d4f389a7e
Alex Lee <hyunho98@gmail.com>
2020-05-04 12:17:39 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,33 @@
 class SongsController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_song!
+    @song = Song.find(params[:id])
+  end
+
+  def song_params
+    params.required(:song).permit(:title, :released, :release_year, :artist_name, :genre)
+  end
 end
