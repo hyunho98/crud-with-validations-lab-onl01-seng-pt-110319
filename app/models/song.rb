b4129c4ec97db2d59828eef145c0539be0cfbe4a
Alex Lee <hyunho98@gmail.com>
2020-05-04 12:06:24 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,10 +1,12 @@
 class Song < ApplicationRecord
   validates :title, presence: true, uniqueness: {scope: %i[release_year artist_name]}
   validates :released, inclusion: {in: [true, false]}
-  validates :release_year, presence: true, if: :is_released?
-  validates :release_year, numericality: {less_than_or_equal_to: Date.current.year}
   validates :artist_name, presence: true
 
+  with_options if: :is_released? do |song|
+    song.validates :release_year, presence: true
+    song.validates :release_year, numericality: {less_than_or_equal_to: Date.current.year}
+  end
   private
 
   def is_released?
