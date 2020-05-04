1afffc50900773d3bc53270af067e52535aacda1
Alex Lee <hyunho98@gmail.com>
2020-05-04 12:00:57 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,7 +2,7 @@ class Song < ApplicationRecord
   validates :title, presence: true, uniqueness: {scope: %i[release_year artist_name]}
   validates :released, inclusion: {in: [true, false]}
   validates :release_year, presence: true, if: :is_released?
-  validates :release_year, numericality: {less_than_or_equal_to: Date.year}
+  validates :release_year, numericality: {less_than_or_equal_to: Date.current.year}
   validates :artist_name, presence: true
 
   private
