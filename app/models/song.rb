86330e6128cb60d6058665dd62d2eac97dbffc3c
Alex Lee <hyunho98@gmail.com>
2020-05-04 12:06:27 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -7,6 +7,7 @@ class Song < ApplicationRecord
     song.validates :release_year, presence: true
     song.validates :release_year, numericality: {less_than_or_equal_to: Date.current.year}
   end
+
   private
 
   def is_released?
