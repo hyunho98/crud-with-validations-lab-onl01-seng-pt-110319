d41c2587234e2c9e26e582a5a0473433596f3133
Alex Lee <hyunho98@gmail.com>
2020-05-04 11:45:36 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,4 +2,6 @@ class Song < ApplicationRecord
   validates :title, presence: true, :repeat_title
   validates :released, inclusion: {[true, false]}
   validates :release_year, presence: true, if: :is_released?
+  validates :release_year, :not_in_the_future
+  validates :artist_name, presence: true
 end
